158484ddb89fa7f7f85fc826ed7c221b9f5b460f
zikalino <zikalino@microsoft.com>
2019-06-21 14:04:02 +0800

    initial fixes in params

--- TemplateAzureCliParams.ts
+++ TemplateAzureCliParams.ts
@@ -28,12 +28,12 @@ export function GenerateAzureCliParams(model: CodeModelCli) : string[] {
         //output.push("        c.argument('location', validator=get_default_location_from_resource_group)");
         //output.push("        c.argument('apimanagement_name', apimanagement_name_type, options_list=['--name', '-n'])");
 
-        output.push("    apimanagement_name_type = CLIArgumentType(options_list='--apimanagement-name-name', help='Name of the Apimanagement.', id_part='name')");
+        output.push("        name_arg_type = CLIArgumentType(options_list=('--name', '-n'), metavar='NAME')");
         output.push("");
         output.push("    with self.argument_context('" + model.GetCliCommand() + "') as c:");
         output.push("        c.argument('tags', tags_type)");
         output.push("        c.argument('location', validator=get_default_location_from_resource_group)");
-        output.push("        c.argument('" + model.GetCliCommand() + "_name', " + model.GetCliCommand() + "_name_type, options_list=['--name', '-n'])");
+        output.push("        c.argument('" + model.GetCliCommand() + "_name', name_arg_type, options_list=['--name', '-n'])");
         
         let methods: string[] = model.GetCliCommandMethods();
         for (let mi = 0; mi < methods.length; mi++)
@@ -41,7 +41,7 @@ export function GenerateAzureCliParams(model: CodeModelCli) : string[] {
             let method: string = methods[mi];
             output.push("");
             output.push("    with self.argument_context('" + model.GetCliCommand() + "') as c:");
-            output.push("        c.argument('" + model.GetCliCommandModuleName() + "_name', " + model.GetCliCommandModuleName() + "_name_type, id_part=None)");
+            output.push("        c.argument('" + model.GetCliCommandModuleName() + "_name', name_arg_type, id_part=None)");
         
             //output.push("");
             //output.push("    with self.argument_context('apimanagement list') as c:");
@@ -56,7 +56,7 @@ export function GenerateAzureCliParams(model: CodeModelCli) : string[] {
     output.push("    with self.argument_context('apimanagement') as c:");
     output.push("        c.argument('tags', tags_type)");
     output.push("        c.argument('location', validator=get_default_location_from_resource_group)");
-    output.push("        c.argument('apimanagement_name', apimanagement_name_type, options_list=['--name', '-n'])");
+    output.push("        c.argument('apimanagement_name', name_arg_type, options_list=['--name', '-n'])");
     
     return output;
 }
--- dist/TemplateAzureCliParams.js
+++ dist/TemplateAzureCliParams.js
@@ -23,18 +23,18 @@ function GenerateAzureCliParams(model) {
         //output.push("        c.argument('tags', tags_type)");
         //output.push("        c.argument('location', validator=get_default_location_from_resource_group)");
         //output.push("        c.argument('apimanagement_name', apimanagement_name_type, options_list=['--name', '-n'])");
-        output.push("    apimanagement_name_type = CLIArgumentType(options_list='--apimanagement-name-name', help='Name of the Apimanagement.', id_part='name')");
+        output.push("        name_arg_type = CLIArgumentType(options_list=('--name', '-n'), metavar='NAME')");
         output.push("");
         output.push("    with self.argument_context('" + model.GetCliCommand() + "') as c:");
         output.push("        c.argument('tags', tags_type)");
         output.push("        c.argument('location', validator=get_default_location_from_resource_group)");
-        output.push("        c.argument('" + model.GetCliCommand() + "_name', " + model.GetCliCommand() + "_name_type, options_list=['--name', '-n'])");
+        output.push("        c.argument('" + model.GetCliCommand() + "_name', name_arg_type, options_list=['--name', '-n'])");
         let methods = model.GetCliCommandMethods();
         for (let mi = 0; mi < methods.length; mi++) {
             let method = methods[mi];
             output.push("");
             output.push("    with self.argument_context('" + model.GetCliCommand() + "') as c:");
-            output.push("        c.argument('" + model.GetCliCommandModuleName() + "_name', " + model.GetCliCommandModuleName() + "_name_type, id_part=None)");
+            output.push("        c.argument('" + model.GetCliCommandModuleName() + "_name', name_arg_type, id_part=None)");
             //output.push("");
             //output.push("    with self.argument_context('apimanagement list') as c:");
             //output.push("        c.argument('apimanagement_name', apimanagement_name_type, id_part=None)");
@@ -46,7 +46,7 @@ function GenerateAzureCliParams(model) {
     output.push("    with self.argument_context('apimanagement') as c:");
     output.push("        c.argument('tags', tags_type)");
     output.push("        c.argument('location', validator=get_default_location_from_resource_group)");
-    output.push("        c.argument('apimanagement_name', apimanagement_name_type, options_list=['--name', '-n'])");
+    output.push("        c.argument('apimanagement_name', name_arg_type, options_list=['--name', '-n'])");
     return output;
 }
 exports.GenerateAzureCliParams = GenerateAzureCliParams;
