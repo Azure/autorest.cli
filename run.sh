54c8f61d23446c01028c017ce24bd7baee774a69
zikalino <zikalino@microsoft.com>
2019-07-02 16:24:21 +0800

    updated copy

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-cp -rf --verbose /dist /autorest.devops
+cp -rf --verbose /source/dist /autorest.devops
+cp -rf --verbose /source/input /autorest.devops
 autorest --devops --use=/autorest.devops --python-sdks-folder=/generated --output-folder=/generated $1
