a77d1e309b43405d8f9882fdf0dd7d88355fbcc6
zikalino <zikalino@microsoft.com>
2019-07-29 15:35:25 +0800

    fixed terraform

--- TemplateMagicModulesTerraformYaml.ts
+++ TemplateMagicModulesTerraformYaml.ts
@@ -3,8 +3,6 @@
 export function GenerateMagicModulesTerraformYaml(model: CodeModel) : string[] {
     var output: string[] = [];
     output.push("--- !ruby/object:Provider::Azure::Terraform::Config");
-    output.push("author: audevbot");
-    output.push("version_added: \"2.9\"");
     output.push("overrides: !ruby/object:Overrides::ResourceOverrides");
     output.push("  " + model.ObjectName + ": !ruby/object:Provider::Azure::Terraform::ResourceOverride");
     output.push("    properties: {}");
