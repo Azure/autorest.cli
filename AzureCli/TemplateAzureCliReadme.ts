1b9e4a2f0a4621b486d1fcbfbee7b801c8c9fa03
Zim Kalinowski <zikalino@microsoft.com>
2019-10-16 16:36:34 +0800

    fixed readme generation

--- AzureCli/TemplateAzureCliReadme.ts
+++ AzureCli/TemplateAzureCliReadme.ts
@@ -3,11 +3,11 @@
 export function GenerateAzureCliReadme(model: CodeModelCli) : string[] {
     var output: string[] = [];
 
-    output.push("Microsoft Azure CLI '" + model.GetCliCommand() + "' Extension");
+    output.push("Microsoft Azure CLI '" + model.GetCliCommandModuleName() + "' Extension");
     output.push("==========================================");
     output.push("");
-    output.push("This package is for the '" + model.GetCliCommand() + "' extension.");
-    output.push("i.e. 'az " + model.GetCliCommand() + "'");
+    output.push("This package is for the '" + model.GetCliCommandModuleName() + "' extension.");
+    output.push("i.e. 'az " + model.GetCliCommandModuleName() + "'");
     output.push("");
 
     return output;
--- dist/AzureCli/TemplateAzureCliReadme.js
+++ dist/AzureCli/TemplateAzureCliReadme.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function GenerateAzureCliReadme(model) {
     var output = [];
-    output.push("Microsoft Azure CLI '" + model.GetCliCommand() + "' Extension");
+    output.push("Microsoft Azure CLI '" + model.GetCliCommandModuleName() + "' Extension");
     output.push("==========================================");
     output.push("");
-    output.push("This package is for the '" + model.GetCliCommand() + "' extension.");
-    output.push("i.e. 'az " + model.GetCliCommand() + "'");
+    output.push("This package is for the '" + model.GetCliCommandModuleName() + "' extension.");
+    output.push("i.e. 'az " + model.GetCliCommandModuleName() + "'");
     output.push("");
     return output;
 }
