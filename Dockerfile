7df099d9b6501d8951ea9fe07040c4d20ca774da
Zim Kalinowski <zikalino@microsoft.com>
2019-11-07 18:52:10 +0800

    remove old files

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM zikalino/autorest.cli.base
-MAINTAINER zikalino
-
-ADD . /autorest.cli
-# RUN cd /autorest.cli; npm install
-ENTRYPOINT ["bash", "/autorest.cli/run.sh"]
--- run.sh
+++
@@ -1,3 +0,0 @@
-cp -rf --verbose /source/dist /autorest.cli
-cp -rf --verbose /source/input /autorest.cli
-autorest --cli --use=/autorest.cli --python-sdks-folder=/generated --output-folder=/generated $1
